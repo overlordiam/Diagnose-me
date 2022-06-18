e3f91868289263b82ec49777b5a0912f757cd25b
suhaas <footballandgermany@gmail.com>
2022-06-18 23:58:43 +0530

    prediction at backend working

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,3 @@
-# from flask import Flask, send_from_directory
-# from flask_restful import Api, Resource, reqparse
-# from flask_cors import CORS #comment this on deployment
-# from api.HelloApiHandler import HelloApiHandler
-# from predict import Detect
-
-# app = Flask(__name__, static_url_path='', static_folder='frontend/build')
-# CORS(app) #comment this on deployment
-# api = Api(app)
-
-# @app.route("/", defaults={'path':''})
-# def serve(path):
-#     return send_from_directory(app.static_folder,'index.html')
-
-# @app.route("/predict", methods=['POST'])
-# def predictRoute():
-#     image = request.json['image']
-#     decodeImage(image, clApp.filename)
-#     result = clApp.classifier.predictiondogcat()
-#     return jsonify(result)
-
-# api.add_resource(HelloApiHandler, '/flask/hello')
-# api.add_resource(Detect, '/predict')
 
 from flask import Flask, request, jsonify
 import os
@@ -42,32 +19,30 @@ class ClientApp:
         self.classifier = Detect(self.filename)
 
 
-# @app.route("/", methods=['GET'])
-# @cross_origin()
-# def home():
-#     return render_template('index.html')
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    return {
+        "res": "working"
+    }
 
 
 @app.route("/predict", methods=['POST'])
 @cross_origin()
 def predictRoute():
-    # image = request.json['image']
     pathOfImage = "images"
     if not os.path.isdir(pathOfImage):
         os.mkdir(pathOfImage)
     target=os.path.join(pathOfImage,'image.png')
-
-    file = request.files['file']
-    base64 = request.files['base64']
+    print("hello")
+    file = request.files.get('file')
+    print(file)
     file.save(target)
     clApp = ClientApp()
-    # decodeImage(base64, clApp.filename)
     result = clApp.classifier.predictiondogcat()
     return jsonify(result)
 
 
-# port = int(os.getenv("PORT"))
 if __name__ == "__main__":
     print("working")
-    # app.run(host='0.0.0.0', port=port)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(port=5000, debug=True)
--- src/pages/diagnosis/diagnosis.js
+++ src/pages/diagnosis/diagnosis.js
@@ -4,55 +4,35 @@ import Predict from '../../predict'
 import Axios from "axios";
 
 function Diagnosis() {
-  const [image, setImage] = useState({ preview: '', data: '', base64: '' })
+  const [image, setImage] = useState({ preview: '', data: ''})
   const [status, setStatus] = useState('')
   const [getMessage, setGetMessage] = useState('')
-  const [img_base64, setImg_base64] = useState('')
 
-  useEffect(() => {
-    Axios.post('http://localhost:5000/predict',{}).then(response => {
-      console.log("SUCCESS", response)
-      setGetMessage(response)
-    }).catch(error => {
-      console.log(error)
-  })
-}, [img_base64])
+//   useEffect(() => {
+// }, [img_base64])
 
-  // function encodeImageFileAsURL(e) {
-  //   var file = e.target.files[0];
-  //   var reader = new FileReader();
-  //   reader.onloadend = function() {
-  //     console.log('RESULT', reader.result)
-  //   }
-  //   return reader.readAsDataURL(file);
-  // }
 
-  
   const handleSubmit = async (e) => {
     e.preventDefault()    
     let formData = new FormData()
     formData.append('file', image.data)
-    // formData.append('base64', image.base64)
-    await Axios.post("http://localhost:5000/predict", formData)
-    .then(response => console.log(response))
-    // setStatus(response.statusText)
-  //   if (response) {
-  //     setStatus(response.statusText)
-     
-  // }
+    
+    const res = await Axios.post("http://localhost:5000/predict", formData, {
+      "Content-Type": "multipart/form-data"
+    })
+    console.log(res);
   }
+  
   const handleFileChange = async (e) => {
     var reader = new FileReader();
     const img = {
       preview: URL.createObjectURL(e.target.files[0]),
-      data: e.target.files[0],
-      base64: img_base64
+      data: e.target.files[0]
     }
 
     setImage(img)
     reader.onloadend = async () => {
-      setImg_base64(() => reader.result);
-      // console.log(img_base64);
+       reader.result;
     }
     reader.readAsDataURL(e.target.files[0]);
     
